581eed4c4c76c44212accab7785a841a3e8e75cc
Floris Huetink <floris@brightin.nl>
2014-12-01 11:36:55 +0100

    Make sure Configuration uses ENV variables on initialize

--- spec/buckaroo_client/configuration_spec.rb
+++ spec/buckaroo_client/configuration_spec.rb
@@ -8,6 +8,20 @@ describe BuckarooClient::Configuration do
     end
   end
 
+  describe '#initialize' do
+    it 'defaults to ENV values if no arguments given' do
+      stub_const('ENV', {
+        'BUCKAROO_CLIENT_WEBSITEKEY' => 'a',
+        'BUCKAROO_CLIENT_SECRET' => 'b',
+        'BUCKAROO_CLIENT_ENVIRONMENT' => 'c'
+      })
+      c = described_class.new
+      expect(c.websitekey).to eq 'a'
+      expect(c.secret).to eq 'b'
+      expect(c.environment).to eq 'c'
+    end
+  end
+
   describe '#websitekey' do
     include_examples 'getter/setter', 'websitekey'
   end
